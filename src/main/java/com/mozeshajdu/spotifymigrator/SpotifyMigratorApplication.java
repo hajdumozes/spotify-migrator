15b8cccf6119e6c5cc547de10cb775b0158875b0
MHajdu <mozes.hajdu@dmgmori-heitec.hu>
2022-05-04 17:07:57 +0200

    initial commit

---
+++ src/main/java/com/mozeshajdu/spotifymigrator/SpotifyMigratorApplication.java
@@ -0,0 +1,13 @@
+package com.mozeshajdu.spotifymigrator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotifyMigratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotifyMigratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mozeshajdu/spotifymigrator/SpotifyMigratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mozeshajdu.spotifymigrator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotifyMigratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

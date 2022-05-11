2b595106e4c3dc608dca29b08292f681274086bd
MHajdu <mozes.hajdu@dmgmori-heitec.hu>
2022-05-11 19:11:57 +0200

    fetch-tags: optional searchparams

--- src/main/java/com/mozeshajdu/spotifymigrator/spotify/service/SpotifyQueryStringGenerator.java
+++ src/main/java/com/mozeshajdu/spotifymigrator/spotify/service/SpotifyQueryStringGenerator.java
@@ -1,22 +1,31 @@
 package com.mozeshajdu.spotifymigrator.spotify.service;
 
 import com.mozeshajdu.spotifymigrator.spotify.entity.SearchParameters;
+import org.apache.logging.log4j.util.Strings;
 import org.springframework.stereotype.Component;
+
+import java.util.Optional;
 
 @Component
 public class SpotifyQueryStringGenerator {
-    public static final String SPOTIFY_TRACK_QUERY_PARAM = "track";
-    public static final String SPOTIFY_ALBUM_QUERY_PARAM = "album";
-    public static final String SPOTIFY_ARTIST_QUERY_PARAM = "artist";
-    public static final String SPOTIFY_YEAR_QUERY_PARAM = "year";
+    public static final String TITLE_FIELD = "track";
+    public static final String ALBUM_FIELD = "album";
+    public static final String ARTIST_FIELD = "artist";
+    public static final String YEAR_FIELD = "year";
     public static final String SPOTIFY_QUERY_DELIMITER = " ";
     public static final String SPOTIFY_SEARCH_PARAM_FORMAT = "%s:%s";
 
     public String generateFrom(SearchParameters searchParameters) {
-        String trackPart = String.format(SPOTIFY_SEARCH_PARAM_FORMAT, SPOTIFY_TRACK_QUERY_PARAM, searchParameters.getTitle());
-        String albumPart = String.format(SPOTIFY_SEARCH_PARAM_FORMAT, SPOTIFY_ALBUM_QUERY_PARAM, searchParameters.getAlbum());
-        String artistPart = String.format(SPOTIFY_SEARCH_PARAM_FORMAT, SPOTIFY_ARTIST_QUERY_PARAM, searchParameters.getArtist());
-        String yearPart = String.format(SPOTIFY_SEARCH_PARAM_FORMAT, SPOTIFY_YEAR_QUERY_PARAM, searchParameters.getYear());
+        String trackPart = getQueryPart(TITLE_FIELD, searchParameters.getTitle());
+        String albumPart = getQueryPart(ALBUM_FIELD, searchParameters.getAlbum());
+        String artistPart = getQueryPart(ARTIST_FIELD, searchParameters.getArtist());
+        String yearPart = getQueryPart(YEAR_FIELD, searchParameters.getYear());
         return String.join(SPOTIFY_QUERY_DELIMITER, trackPart, albumPart, artistPart, yearPart);
+    }
+
+    private String getQueryPart(String field, String fieldValue) {
+        return Optional.ofNullable(fieldValue)
+                .map(value -> String.format(SPOTIFY_SEARCH_PARAM_FORMAT, field, value))
+                .orElse(Strings.EMPTY);
     }
 }
